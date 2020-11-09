64622a15276a562ca70e59dc4a49a5f202cb0fdb
Praveen Kamath <kamathp@synopsys.com>
2020-11-09 14:21:06 +0530

    added dice roll logic

--- app.js
+++ app.js
@@ -12,17 +12,46 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('join', (query) => {
         if(!rooms[query.room_id]) {
-            rooms[query.room_id] = {};
-            rooms[query.room_id][query.player_id] = {score: 0, 'player_no': 1};
+            rooms[query.room_id] = {
+                players: [query.player_id],
+                game: {}
+            };
+            rooms[query.room_id].game[query.player_id] = {score: 0, 'player_no': 1, 'turn': true, 'win': false};
         } else {
-            rooms[query.room_id][query.player_id] = {score: 0, 'player_no': Object.keys(rooms[query.room_id]).length+1};
-            var game = rooms[query.room_id];
+            rooms[query.room_id].players.push(query.player_id);
+            rooms[query.room_id].game[query.player_id] = {score: 0, 'player_no': Object.keys(rooms[query.room_id]).length+1, 'turn': false, 'win': false};
+            var game = rooms[query.room_id].game;
             for(var player_id in game) {
                 console.log('starting game '+ player_id)
                 io.emit(player_id + '-game-start', game);
             }
         }
         console.log(rooms)
+    });
+    socket.on('roll', (query) => {
+        let dice_val = Math.floor(Math.random() * Math.floor(5)) + 1;
+        let player_id = rooms[query.room_id].players[query.player_no-1];
+        let game = rooms[query.room_id].game;
+        let game_win = false;
+        let current_player = game[player_id];
+        current_player.score = current_player.score + dice_val;
+        if(current_player.score >= 61) {
+            current_player.win = true;
+            game_win = true;
+        }
+        current_player.turn = false;
+        if(!game_win) {
+            //set next player turn
+            let next_player_id;
+            if(query.player_no == rooms[query.room_id].players.length) {
+                next_player_id = rooms[query.room_id].players[0];
+            } else {
+                next_player_id = rooms[query.room_id].players[query.player_no];
+            }
+            game[next_player_id].turn = true;
+        }
+        console.log(JSON.stringify(rooms));
+        io.emit('roll-result', game);
     });
 });
 
